61574bea1fc6ab00744207807e2afdd91e743836
devrozz <rozsypal.dom@gmail.com>
2020-05-09 12:11:35 +0200

    Create basic components, service and create header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { WeatherDetailComponent } from './weather-detail/weather-detail.component';
+import { WeatherService } from './weather.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    WeatherDetailComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    WeatherService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
